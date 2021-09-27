1aa03ce5a50d31b32b00bbb0cb5894c89634daaf
choco-tyranno <choco.tyranno@gmail.com>
2021-09-27 21:41:31 +0900

    fix: change retrofit test code.

--- MaskLocation/app/src/main/java/com/choco_tyranno/masklocation/repository/MaskService.kt
+++ MaskLocation/app/src/main/java/com/choco_tyranno/masklocation/repository/MaskService.kt
@@ -1,4 +1,17 @@
 package com.choco_tyranno.masklocation.repository
 
+import com.choco_tyranno.masklocation.model.StoreInfo
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+
 interface MaskService {
+    @GET("storesByGeo/json/")
+    fun fetchStoreInfo(
+    ): Call<StoreInfo>
+    companion object{
+        const val BASE_URL = "https://gist.githubusercontent.com/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/"
+    }
 }
--- MaskLocation/app/src/test/java/com/choco_tyranno/masklocation/ExampleUnitTest.kt
+++ MaskLocation/app/src/test/java/com/choco_tyranno/masklocation/ExampleUnitTest.kt
@@ -1,8 +1,13 @@
 package com.choco_tyranno.masklocation
 
+import com.choco_tyranno.masklocation.model.StoreInfo
+import com.choco_tyranno.masklocation.repository.MaskService
 import org.junit.Test
 
 import org.junit.Assert.*
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +18,20 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun retrofitTest(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl(MaskService.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+
+        val service: MaskService = retrofit.create(MaskService::class.java)
+        val storeInfoCall :Call<StoreInfo> = service.fetchStoreInfo()
+        val storeInfo : StoreInfo? = storeInfoCall.execute().body()
+        assertEquals(222, storeInfo?.count)
+        assertEquals(222, storeInfo?.stores?.size)
+
     }
 }
